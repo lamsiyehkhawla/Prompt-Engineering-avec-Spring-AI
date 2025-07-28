134b02759238c463516f22a2ace8882abb980ad4
khawla lamsiyeh <61538329+khawlalam@users.noreply.github.com>
2025-07-28 19:10:46 +0000

    second commit

--- src/main/java/ma/enset/bdccai/controllers/AIAgentController.java
+++ src/main/java/ma/enset/bdccai/controllers/AIAgentController.java
@@ -1,7 +1,9 @@
 package ma.enset.bdccai.controllers;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AIAgentController {
     private ChatClient chatClient;
 
-    public AIAgentController(ChatClient.Builder builder) {
+    public AIAgentController(ChatClient.Builder builder, ChatMemory memory) {
         this.chatClient = builder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(memory).build())
                 .build();
     }
     @GetMapping("/chat")
     public String askLLM(@RequestParam String query) {
         try {
             return chatClient.prompt()
+                    .system("repond toujours en majuscule")
                     .user(query).call().content();
         } catch (Exception e) {
             return "Erreur lors de l'appel à OpenAI : " + e.getMessage();
